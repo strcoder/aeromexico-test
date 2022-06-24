9d9f6a9dcec026186c085809922d91071dcd866a
Antonio Guzman <aguzmanv09@gmail.com>
2022-06-24 13:05:34 -0500

    Create CharacterCard UI

---
+++ src/components/CharacterCard/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { HiOutlineBookmark, HiBookmark } from 'react-icons/hi';
+import './styles.scss';
+
+type CharacterCardProps = {
+  character: {
+    name?: string;
+    species?: string;
+    gender?: string;
+    house?: string;
+    dateOfBirth?: string;
+    yearOfBirth?: number;
+    ancestry?: string;
+    eyeColour?: string;
+    hairColour?: string;
+    wand?: {
+      wood: string;
+      core: string;
+      length: number
+    };
+    patronus?: string;
+    hogwartsStudent?: boolean;
+    hogwartsStaff?: boolean;
+    actor?: string;
+    alive?: boolean;
+    image?: string;
+  };
+};
+
+const CharacterCard = ({ character }: CharacterCardProps) => {
+  const [favorite, setFavorite] = useState<boolean>(false);
+  return (
+    <div className='CharacterCard'>
+      <div className={`CharacterCard__header ${character.house}`}>
+        <figure className='CharacterCard__header--image'>
+          <img src={character.image} alt={character.name} />
+        </figure>
+      </div>
+      <div className='CharacterCard__body'>
+        <div className='CharacterCard__body--status'>
+          <div>
+            <p>{character.alive ? 'Vivo' : 'Finado'}</p>
+            <p>{character.hogwartsStudent ? 'Estudiante' : character.hogwartsStaff ? 'Staff' : ''}</p>
+          </div>
+          <div>
+            <button
+              type='button'
+              className={`btn-link ${favorite}`}
+              onClick={() => setFavorite(!favorite)}
+            >
+              {favorite ? <HiBookmark /> : <HiOutlineBookmark />}
+            </button>
+          </div>
+        </div>
+        <div className='CharacterCard__body--title'>
+          <p><strong>{character.name}</strong></p>
+        </div>
+        <div className='CharacterCard__body--info'>
+          <p>
+            <strong>Cumpleaños:</strong>
+            {' '}
+            {character.dateOfBirth}
+          </p>
+          <p>
+            <strong>Género:</strong>
+            {' '}
+            {character.gender}
+          </p>
+          <p>
+            <strong>Color de ojos:</strong>
+            {' '}
+            {character.eyeColour}
+          </p>
+          <p>
+            <strong>Color de pelo:</strong>
+            {' '}
+            {character.hairColour}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterCard;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import CharacterCard from '../../components/CharacterCard';
+import { getCharacters } from '../../redux/actions';
 
-const Home = () => {
+const Home = ({ getCharacters, characters }) => {
+
+  useEffect(() => {
+    if (!characters || characters.length === 0) {
+      getCharacters();
+    }
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      {characters && characters.length !== 0 && (
+        <>
+          {characters.map((character) => (
+            <CharacterCard
+              character={character}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    characters: state.characters,
+  };
+};
+
+const mapDispatchToProps = {
+  getCharacters,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
